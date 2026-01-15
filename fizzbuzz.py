0548225d8e0a68d82f59a454e11969c1e72c8808
VictorPiggy <samgookgy3@gmail.com>
2026-01-15 17:39:30 +0900

    feat: Creat file and prinit fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 
-result = ["fizz"*(i % 3 == 0) + "buzz"*(i % 5 == 0) or i for i in range(1, 16)]
+result = ["fizz"*(i % 3 == 0) or i for i in range(1, 16)]
+
+#result = ["fizz"*(i % 3 == 0) + "buzz"*(i % 5 == 0) or i for i in range(1, 16)]
 
 #result = ["fizzbuzz" if i % 15 == 0 else "fizz" if i % 3 == 0 else "buzz" if i % 5 == 0 else i for i in range(1, 31)]
 
